185a25082e7db32802dbadac27b1b6df8576c674
Corey McCue <coreysmccue@gmail.com>
2022-04-09 11:00:18 +0200

    add initial back end api calls

--- upcycle-backend/server.js
+++ upcycle-backend/server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/listings", (req, res) => {
+  res.json(products);
+});
+app.get("/api/listings/:id", (req, res) => {
+  const product = products.find((p) => p.id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, console.log("Server started on port 5000"));
